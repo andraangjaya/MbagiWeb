9eff311680423ab3254328de717d1a8c59b82547
andra <andrabaktiangjaya@gmail.com>
2026-05-19 18:36:45 +0800

    Created view detail page

---
+++ src/app/pages/view-detail-page/view-detail-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-detail-page',
+  imports: [],
+  templateUrl: './view-detail-page.component.html',
+  styleUrl: './view-detail-page.component.css',
+})
+export class ViewDetailPageComponent {}
